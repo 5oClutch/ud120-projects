8d902e923c9a985b1e3e932b1e5eb5be1210bc02
5oClutch <emailtonisarg@gmail.com>
2018-05-19 10:12:25 -0400

    Naive Bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -33,7 +33,11 @@ clf = GaussianNB()
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
 pred = clf.predict(features_test)
+print "Predicition Time:", round(time()-t1, 3), "s"
+
 print(clf.score(features_test, labels_test))
 
 #########################################################
